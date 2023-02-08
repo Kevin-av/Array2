3e0f25fceaa5fee1bf4ac2bfa98241a0dde7dec0
Jhon-g26 <kiavila@sudamericano.edu.ec>
2023-02-08 11:43:47 -0500

    aa

---
+++ image.js
@@ -0,0 +1,12 @@
+let doc = document.getElementById("img01");
+
+let url = prompt ('ingresar la imagen');
+//https://drive.google.com/file/d/1RO97Yu3oMIvcM4M5Lq-AAs_Iem1Mo0cR/view
+
+for(let i=0; i<url.length; i++){
+
+    console.log(url.substring(i, i+2));
+
+}
+
+doc.src=`https://drive.google.com/uc?export=view&id=${url}`;
