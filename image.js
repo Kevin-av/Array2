968ce6fd41a6dd3925d50c55f13771d999681ccf
Jhon-g26 <kiavila@sudamericano.edu.ec>
2023-02-08 11:57:40 -0500

    ERROR

--- image.js
+++ image.js
@@ -3,16 +3,22 @@ let doc = document.getElementById("img01");
 let url = prompt ('ingresar la imagen');
 //https://drive.google.com/file/d/1RO97Yu3oMIvcM4M5Lq-AAs_Iem1Mo0cR/view
 
-for(let i=0; i<url.length; i++){
+let contador = 0;
+let id = "";
 
+for(let i=0; i<url.length; i++){
     console.log(url.substring(i, i+2));
+    if(url[i] == "/"){
+        contador++;
+        console.log(contador);
+    }
 
+    if(contador == 5){
+        id+=url[i];
+
+    }
 }
 
-for(let i=0; i<url.length; i++){
-
-let result = url.substring(30,65);
-
-}
+console.log("id"+id);
 
 doc.src=`https://drive.google.com/uc?export=view&id=${url}`;
